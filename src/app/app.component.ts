640b2cdf0fc68ac84bbb8b4d60ee27e909aaaee0
patricio-jp <patojimenezpaz@gmail.com>
2024-12-19 17:32:22 -0300

    Add components and routing by url to component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,82 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import {
+  NavigationEnd,
+  Router,
+  RouterLink,
+  RouterLinkActive,
+} from '@angular/router';
+import { filter } from 'rxjs';
+import { SkillsComponent } from './sections/skills/skills.component';
+import { ProjectsComponent } from './sections/projects/projects.component';
+import { ResumeComponent } from './sections/resume/resume.component';
+import { ContactComponent } from './sections/contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    SkillsComponent,
+    ProjectsComponent,
+    ResumeComponent,
+    ContactComponent,
+  ],
   templateUrl: './app.component.html',
   styles: [],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'patriciojp-portfolio';
+
+  private router = inject(Router);
+
+  constructor() {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.scrollToSection();
+      });
+  }
+
+  ngOnInit(): void {
+    // Auto dark-mode
+    document.documentElement.classList.toggle(
+      'dark',
+      localStorage['theme'] === 'dark' ||
+        (!('theme' in localStorage) &&
+          window.matchMedia('(prefers-color-scheme: dark)').matches)
+    );
+  }
+
+  scrollToSection(): void {
+    const currentRoute = this.router.url;
+
+    let compSelector = '';
+
+    switch (currentRoute) {
+      case '/skills': {
+        compSelector = 'app-skills';
+        break;
+      }
+      case '/projects': {
+        compSelector = 'app-projects';
+        break;
+      }
+      case '/resume': {
+        compSelector = 'app-resume';
+        break;
+      }
+      case '/contact': {
+        compSelector = 'app-contact';
+        break;
+      }
+      default: {
+        compSelector = 'app-root';
+        break;
+      }
+    }
+
+    const element = document.querySelector(compSelector);
+    if (element) element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+  },
+  {
+    path: 'skills',
+    component: AppComponent,
+  },
+  {
+    path: 'projects',
+    component: AppComponent,
+  },
+  {
+    path: 'resume',
+    component: AppComponent,
+  },
+  {
+    path: 'contact',
+    component: AppComponent,
+  },
+];
